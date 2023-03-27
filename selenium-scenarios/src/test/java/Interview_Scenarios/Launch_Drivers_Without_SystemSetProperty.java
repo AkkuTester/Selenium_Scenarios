5affb9b933d176b1ca566958b929284ba1c0a552
HP <HP@DESKTOP-BTQ2T4D>
2023-03-27 15:02:24 +0530

    Real Interview Scenarios

---
+++ selenium-scenarios/src/test/java/Interview_Scenarios/Launch_Drivers_Without_SystemSetProperty.java
@@ -0,0 +1,25 @@
+package Interview_Scenarios;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Launch_Drivers_Without_SystemSetProperty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//	WebDriver driver = new EdgeDriver(); //using edge
+		
+		//using chrome
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("--remote-allow-origins=*");
+	
+	WebDriver driver = new ChromeDriver(options);
+	driver.get("https://www.google.com/");
+
+	
+	}
+
+}
