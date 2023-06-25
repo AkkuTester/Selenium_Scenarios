376cdfb94f9cb1b70ad16e0b681fed993f78746c
Akku <122626835+AkkuTester@users.noreply.github.com>
2023-06-25 13:25:37 +0530

    Capture Screenshot Whenever Test Case Failed
    Dynamically Capture Screenshot Whenever Test Case Failed

---
+++ selenium-scenarios/src/test/java/Interview_Scenarios/ScreenShotTest.java
@@ -0,0 +1,58 @@
+package takeSS;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeMethod;
+
+import web_Common_Functions.CaptureScreenshot;
+
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+public class ScreenShotTest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+	driver	= new EdgeDriver();
+	}
+	
+	
+	@Test
+	public void testCase1() throws InterruptedException {
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		Thread.sleep(1500);
+		Assert.assertTrue(false);
+	}
+
+	@Test
+	public void testCase2() throws InterruptedException {
+		driver.get("https://www.instagram.com/");
+		Thread.sleep(1500);
+		Assert.assertTrue(false);
+	}
+	
+	@Test
+	public void testCase3() {
+		
+		driver.get("https://www.linkedin.com/");
+	
+	}
+	
+	@AfterMethod
+	public void shutDown(ITestResult result) throws IOException, InterruptedException {
+		//if test case failed
+		if(ITestResult.FAILURE == result.getStatus()) {
+				CaptureScreenshot.captureScreenshot(driver, result.getName());
+			System.out.println("Test Failed.. Screenshot captured");
+			Thread.sleep(1500);
+			driver.close();
+		}
+	}
+}
